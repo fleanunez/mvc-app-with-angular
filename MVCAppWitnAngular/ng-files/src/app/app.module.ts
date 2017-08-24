3bacb8a81a21893258822b633d97c5fceebe1682
Chris Faustin <chris.faustin@gmail.com>
2017-08-24 09:44:52 -0400

    create nav and page-not-fount components

--- MVCAppWitnAngular/ng-files/src/app/app.module.ts
+++ MVCAppWitnAngular/ng-files/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppsListComponent } from './apps-list/apps-list.component';
+import { RoutingModule } from './app.routing.module';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      AppsListComponent,
+      HomeComponent,
+      PageNotFoundComponent,
+      NavComponent
   ],
   imports: [
+    RoutingModule,
     BrowserModule
   ],
   providers: [],
---
+++ MVCAppWitnAngular/ng-files/src/app/app.routing.module.ts
@@ -0,0 +1,13 @@
+﻿import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppsListComponent } from './apps-list/apps-list.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    { path: 'apps', component: AppsListComponent },
+    { path: '', component: HomeComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+export const RoutingModule: ModuleWithProviders = RouterModule.forRoot(routes, { enableTracing: true });
